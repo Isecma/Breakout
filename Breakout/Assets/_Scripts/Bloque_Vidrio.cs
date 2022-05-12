6f0fda0d83d7562a11da3b1f925d823d1242c83c
Ismael Echegaray <ismael.echegarayms@hotmail.com>
2022-05-11 19:16:13 -0500

    Creación de proyecto Breakout
    -Se creó nuevo prototipo de proyecto de juego "Breakout"
    -Se crearon 5 bloques de diferentes materiales
    -Se creó la bola del juego
    -Se creó la barra horizontal que controla el jugador

---
+++ Breakout/Assets/_Scripts/Bloque_Vidrio.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque_Vidrio : Bloque
+{
+    //Bloque el doble de largo
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistencia = 2;
+    }
+
+}
